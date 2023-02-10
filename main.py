374c72872b14442c56e3c51c7d1f879ff6dccb94
Yurii Yasnitskyi <yura.yasnitskiy@gmail.com>
2023-02-10 11:17:05 +0200

    some new changes

--- main.py
+++ main.py
@@ -18,8 +18,9 @@
 # git config --global user.name "Yurii Yasnitskyi"
 # git config --global user.email "yura.yasnitskiy@gmail.com"
 
-# git config --global core.editor "C:\Users\uraya\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-
+# https://docs.github.com/en/get-started/getting-started-with-git/associating-text-editors-with-git
+# вказування редакору для коментарів
+# git config --global core.editor "code --wait"
 
 # alias
 # Скоротити якусь команду під іншу псевдоназву
@@ -30,7 +31,30 @@
 
 # *** СТВОРЕННЯ РЕПОЗИТОРІЮ ***
 
+# Є 3 серидовище в яких міститься інфомраці про проєкт
+# 1. Це наша робоча папка (working directory)
+# 2. Index
+# 3. Repository
+
+
 # git status
 
+# проіндексувати цей файл
 # git add main.py
+
+# git commit
+
+# Заголовок
+#
+# *дія 1
+# *дія 2
+
+
+# Додати усі файли з проєкту
+# git add .
+
+# .idea містить службові файли програми в якій ми працюємо
+# їх не потрбіно зберігати у нашому репозиторії
+
+# git restore --staged прибрати їх з index
 
