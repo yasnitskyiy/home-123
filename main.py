5be4f5a9fb8a18af1d8c9b36303784cebae20f2a
Yurii Yasnitskyi <yura.yasnitskiy@gmail.com>
2023-02-10 10:45:13 +0200

    First commit
    *hard word
    *pay me

---
+++ main.py
@@ -0,0 +1,36 @@
+# git init
+
+# Для командної роботи
+
+# git config user.name "name"
+# git config user.email "email"
+
+
+# Доступаємось до файлика з налаштуваннями
+# core, user - секції, що в них це значення
+
+# cat .git/config
+
+# є системні налаштування --system
+# --globals на рівні користувача (для ніків краще, бо нові проєкти їх підтягують)
+# --local на рівні проєкту (default)
+
+# git config --global user.name "Yurii Yasnitskyi"
+# git config --global user.email "yura.yasnitskiy@gmail.com"
+
+# git config --global core.editor "C:\Users\uraya\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+
+
+# alias
+# Скоротити якусь команду під іншу псевдоназву
+# git config --global alias.c 'config'
+# git config --global alias.TEXT '!git ...; git ...'
+
+
+
+# *** СТВОРЕННЯ РЕПОЗИТОРІЮ ***
+
+# git status
+
+# git add main.py
+
